f49664a658b88a0d4d69e04ca869472cf5730982
Mohitha-Pallapu <mohithapallapu126@gmail.com>
2025-11-14 21:38:40 +0530

    Create named_routes.dart

---
+++ named_routes.dart
@@ -0,0 +1,48 @@
+import "package:flutter/material.dart";
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      initialRoute:'/',
+      routes:{
+        '/':(context)=>HomeScreen(),
+        '/second':(context)=>SecondScreen()
+      },
+    );
+  }
+}
+class HomeScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar:AppBar(title:Text("Home Screen")),
+      body:Center(
+        child:ElevatedButton(
+          child:Text("Go to Second Screen"),
+          onPressed:(){
+            Navigator.pushNamed(context,'/second');
+          }
+        ),
+      ),
+    );
+  }
+}
+class SecondScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar:AppBar(title:Text("Second Screen")),
+      body:Center(
+        child:ElevatedButton(
+          child:Text("Go Back"),
+          onPressed:(){
+            Navigator.pop(context);
+          }
+        ),
+      ),
+    );
+  }
+}
