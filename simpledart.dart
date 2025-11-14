c7ac9f20546d97e05887fbe9712c8948320ce9d3
Mohitha-Pallapu <mohithapallapu126@gmail.com>
2025-11-14 21:22:32 +0530

    Create simpledart.dart

---
+++ simpledart.dart
@@ -0,0 +1,16 @@
+void main(){
+	int age=5;
+	String name="Zoya";
+	print("Hi!!,I am $name. I am $age years old.");
+	if(age>=18){
+		print("You can drive");
+	}
+	else{
+		print("You can't drive");
+	}
+	for(int i=0;i<5;i++){
+		print("$i");
+	}
+	int add(int a,int b)=>a+b;
+	print("Sum=${add(5,6)}");
+}
